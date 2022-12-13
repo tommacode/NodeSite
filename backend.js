782ad5a278795f345428f38b1741ac3adb13fe95
Tom <tbtom.blake36@gmail.com>
2022-12-13 20:57:45 +0000

    Bug Fixes
    Fixed likes to now work with project ID so if the article title changes the liked status will still be the same. Also changes to the management endpoints where they were using the wrong verification method

--- backend.js
+++ backend.js
@@ -35,13 +35,15 @@ const CommentLimit = rateLimit({
   message: 429, //Send too many requests error
 });
 
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_IP,
-  user: process.env.DB_User,
-  password: process.env.DB_Password,
-  database: process.env.DB_Name,
-}).promise();
+const pool = mysql
+  .createPool({
+    connectionLimit: 10,
+    host: process.env.DB_IP,
+    user: process.env.DB_User,
+    password: process.env.DB_Password,
+    database: process.env.DB_Name,
+  })
+  .promise();
 
 function Logs(req, StatusCode) {
   const date = new Date();
@@ -70,24 +72,24 @@ function Logs(req, StatusCode) {
     path,
     StatusCode,
   ];
-  pool.query(sql, values)
+  pool.query(sql, values);
 }
 
 async function Authorised(cookie, pool) {
   if (cookie == undefined) {
-    return false
+    return false;
   }
-  let sql = `SELECT UserID FROM Sessions WHERE Cookie = ?`
-  let [result] = await pool.query(sql, [cookie])
+  let sql = `SELECT UserID FROM Sessions WHERE Cookie = ?`;
+  let [result] = await pool.query(sql, [cookie]);
   if (result[0].UserID == undefined) {
-    return false
+    return false;
   }
-  result = result[0].UserID
-  sql = 'SELECT Sudo FROM Users WHERE ID = ?'
-  let [sudo] = await pool.query(sql, [result])
-  sudo = sudo[0].Sudo
+  result = result[0].UserID;
+  sql = "SELECT Sudo FROM Users WHERE ID = ?";
+  let [sudo] = await pool.query(sql, [result]);
+  sudo = sudo[0].Sudo;
   if (sudo == 1) {
-    return true
+    return true;
   }
 }
 
@@ -95,7 +97,10 @@ async function GetUserID(cookie) {
   if (cookie == undefined) {
     return null;
   }
-  const [result] = await pool.query('SELECT UserID FROM Sessions WHERE cookie = ?', [cookie])
+  const [result] = await pool.query(
+    "SELECT UserID FROM Sessions WHERE cookie = ?",
+    [cookie]
+  );
   return result[0].UserID;
 }
 
@@ -146,42 +151,42 @@ app.get("/favicon.ico", (req, res) => {
   Logs(req, 200);
 });
 
-app.get('/SignUp', (req, res) => {
+app.get("/SignUp", (req, res) => {
   res.sendFile(__dirname + "/Pages/signUp.html");
   Logs(req, 200);
-})
+});
 
-app.get('/Login', (req, res) => {
+app.get("/Login", (req, res) => {
   res.sendFile(__dirname + "/Pages/login.html");
   Logs(req, 200);
 });
 
-app.get('/Logout', (req, res) => {
-  res.clearCookie('Auth');
-  res.redirect('/');
+app.get("/Logout", (req, res) => {
+  res.clearCookie("Auth");
+  res.redirect("/");
   Logs(req, 200);
-})
+});
 
-app.get('/myAccount', async (req, res) => {
+app.get("/myAccount", async (req, res) => {
   if (GetUserID(req.cookies.Auth) == null) {
-    res.redirect('/Login')
+    res.redirect("/Login");
   } else {
     res.sendFile(__dirname + "/Pages/myAccount.html");
     Logs(req, 200);
   }
-})
+});
 
 //API responses
 //General
 app.get("/api/projects", async (req, res) => {
   const cookie = req.cookies;
   let sql;
-  if (cookie["Auth"] == process.env.ManagementToken) {
+  if (await Authorised(cookie["Auth"], pool)) {
     sql = "SELECT Time,Title,Appetizer,Status FROM Projects";
   } else {
     sql = `SELECT Time,Title,Appetizer,Status FROM Projects WHERE Status = 1`;
   }
-  const [result] = await pool.query(sql)
+  const [result] = await pool.query(sql);
   res.send(result);
   Logs(req, 200);
 });
@@ -191,14 +196,14 @@ app.get("/api/projects/:project", async (req, res) => {
   project = project.replaceAll("-", " ");
   const cookies = req.cookies;
   let sql = `SELECT ID,Time,Title,Appetizer,Content,Likes FROM Projects WHERE Title = ? AND Status = 1`;
-  const [result] = await pool.query(sql, [project])
-  const UserID = await GetUserID(cookies["Auth"])
-  sql = `SELECT count(*) FROM projectLikes WHERE UserID = ? AND Project = ?`
-  const [liked] = await pool.query(sql, [UserID, project])
+  const [result] = await pool.query(sql, [project]);
+  const UserID = await GetUserID(cookies["Auth"]);
+  sql = `SELECT count(*) FROM projectLikes WHERE UserID = ? AND Project = ?`;
+  const [liked] = await pool.query(sql, [UserID, result[0].ID]);
   if (liked[0]["count(*)"] == 1) {
-    result[0].Liked = true
+    result[0].Liked = true;
   } else {
-    result[0].Liked = false
+    result[0].Liked = false;
   }
   res.send(result);
   Logs(req, 200);
@@ -217,30 +222,41 @@ app.get("/photos/:photo", (req, res) => {
 
 //Likes
 app.get("/api/projects/:project/like", async (req, res) => {
-  const UserID = await GetUserID(req.cookies["Auth"])
+  const UserID = await GetUserID(req.cookies["Auth"]);
   if (UserID != null) {
     let project = req.params.project;
     project = project.replaceAll("-", " ");
-    let sql = `UPDATE Projects SET Likes = Likes + 1 WHERE Title = "${project}"`;
-    pool.query(sql)
-    sql = `INSERT INTO projectLikes (Project, UserID) VALUES (?, ?)`;
-    pool.query(sql, [project, UserID])
-    res.sendStatus(204);
-    Logs(req, 204);
+    //Check if user has already liked the project
+    let sql = `SELECT count(*) FROM projectLikes WHERE UserID = ? AND Project = ?`;
+    let [result] = await pool.query(sql, [UserID, req.params.project]);
+    if (result[0]["count(*)"] < 1) {
+      [result] = await pool.query(`SELECT ID FROM Projects WHERE Title = ?`, [
+        project,
+      ]);
+      sql = `UPDATE Projects SET Likes = Likes + 1 WHERE Title = "${project}"`;
+      pool.query(sql);
+      sql = `INSERT INTO projectLikes (Project, UserID) VALUES (?, ?)`;
+      pool.query(sql, [result[0].ID, UserID]);
+      res.sendStatus(204);
+      Logs(req, 204);
+    }
   }
 });
-
-
 
 app.get("/api/projects/:project/dislike", async (req, res) => {
   const UserID = await GetUserID(req.cookies["Auth"]);
   if (UserID != null) {
     let project = req.params.project;
     project = project.replaceAll("-", " ");
-    let sql = `UPDATE Projects SET Likes = Likes - 1 WHERE Title = "${project}"`;
-    pool.query(sql)
+    let sql = `SELECT count(*) FROM projectLikes WHERE UserID = ? AND Project = ?`;
+    let [result] = await pool.query(sql, [UserID, req.params.project]);
+    [result] = await pool.query(`SELECT ID FROM Projects WHERE Title = ?`, [
+      project,
+    ]);
+    sql = `UPDATE Projects SET Likes = Likes - 1 WHERE Title = "${project}"`;
+    pool.query(sql);
     sql = `DELETE FROM projectLikes WHERE Project = ? AND UserID = ?`;
-    pool.query(sql, [project, UserID])
+    pool.query(sql, [result[0].ID, UserID]);
     res.sendStatus(204);
     Logs(req, 204);
   }
@@ -263,7 +279,7 @@ app.post("/api/projects/:project/comment", CommentLimit, async (req, res) => {
   const UserID = await GetUserID(req.cookies["Auth"]);
   const sql = `INSERT INTO Comments (Project,UserID,Content,Time,Likes,Unique_id) VALUES (?, ?, ?, ?, ?, ?)`;
   const values = [project, UserID, comment, datetime, 0, id];
-  pool.query(sql, values)
+  pool.query(sql, values);
   res.sendStatus(204);
   Logs(req, 204);
 });
@@ -273,7 +289,7 @@ app.get("/api/projects/:project/comments", async (req, res) => {
   let project = req.params.project;
   project = project.replaceAll("-", " ");
   let sql = `SELECT UserID,Content,Likes,Unique_id FROM Comments Where Project = ? ORDER BY FIELD(Unique_id, ?) DESC, Likes DESC`;
-  let [result] = await pool.query(sql, [project, req.cookies.comment])
+  let [result] = await pool.query(sql, [project, req.cookies.comment]);
   //Convert the UserID to the username
   for (const comment of result) {
     sql = `SELECT Username FROM Users WHERE ID = ?`;
@@ -282,15 +298,15 @@ app.get("/api/projects/:project/comments", async (req, res) => {
     delete comment.UserID;
   }
   //If the user is logged in then check if they liked the comment
-  if (await GetUserID(req.cookies["Auth"]) != null) {
+  if ((await GetUserID(req.cookies["Auth"])) != null) {
     const UserID = await GetUserID(req.cookies["Auth"]);
     for (const comment of result) {
-      sql = `SELECT count(*) FROM commentLikes WHERE UserID = ? AND Unique_id = ?`
-      const [liked] = await pool.query(sql, [UserID, comment.Unique_id])
+      sql = `SELECT count(*) FROM commentLikes WHERE UserID = ? AND Unique_id = ?`;
+      const [liked] = await pool.query(sql, [UserID, comment.Unique_id]);
       if (liked[0]["count(*)"] == 1) {
-        comment.Liked = true
+        comment.Liked = true;
       } else {
-        comment.Liked = false
+        comment.Liked = false;
       }
     }
   }
@@ -300,26 +316,26 @@ app.get("/api/projects/:project/comments", async (req, res) => {
 
 //Comment likes
 app.get("/api/comments/:id/like", async (req, res) => {
-  const UserID = await GetUserID(req.cookies["Auth"])
+  const UserID = await GetUserID(req.cookies["Auth"]);
   if (UserID != null) {
     const id = req.params.id;
     let sql = `UPDATE Comments SET Likes = Likes + 1 WHERE unique_id = ?`;
-    pool.query(sql, [id])
+    pool.query(sql, [id]);
     sql = `INSERT INTO commentLikes (Unique_id, UserID) VALUES (?, ?)`;
-    pool.query(sql, [id, UserID])
+    pool.query(sql, [id, UserID]);
     res.sendStatus(204);
     Logs(req, 204);
   }
 });
 
 app.get("/api/comments/:id/dislike", async (req, res) => {
-  const UserID = await GetUserID(req.cookies["Auth"])
+  const UserID = await GetUserID(req.cookies["Auth"]);
   if (UserID != null) {
     const id = req.params.id;
     let sql = `UPDATE Comments SET Likes = Likes - 1 WHERE unique_id = ?`;
-    pool.query(sql, [id])
+    pool.query(sql, [id]);
     sql = `DELETE FROM commentLikes WHERE unique_id = ? AND UserID  = ?`;
-    pool.query(sql, [id, UserID])
+    pool.query(sql, [id, UserID]);
     res.sendStatus(204);
     Logs(req, 204);
   }
@@ -358,12 +374,13 @@ app.post("/api/projects/new", async (req, res) => {
     const date = new Date();
     const datetime = date.toISOString().slice(0, 19).replace("T", " ");
     const sql = `INSERT INTO Projects (Time, Title, Appetizer, Content, Tags, Status, Likes) VALUES (?, ?, ?, ?, ?, ?, 0)`;
-    await pool.query(sql, [datetime, title, appetizer, Content, tags, Status])
+    await pool
+      .query(sql, [datetime, title, appetizer, Content, tags, Status])
       .then(() => {
         res.send(
           `Successfully added project the visibility is set to ${Status}`
         );
-      })
+      });
     Logs(req, 200);
   } else {
     res.sendStatus(401);
@@ -390,7 +407,7 @@ app.post("/api/projects/:id/edit", async (req, res) => {
     //Rewrite statment to use prepared statements
     let sql = `UPDATE Projects SET Title = ?, Appetizer = ?, Content = ?, Tags = ? WHERE id = ?`;
     pool.query(sql, [title, appetizer, content, tags, id]);
-    res.send("Successfully updated project")
+    res.send("Successfully updated project");
   } else {
     res.sendStatus(403);
     Logs(req, 403);
@@ -439,7 +456,8 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/api/projects/:project/visibility", (req, res) => {
-  const project = req.params.project;
+  let project = req.params.project;
+  project = project.replaceAll("-", " ");
   const cookies = req.cookies;
   if (Authorised(cookies["Auth"], pool)) {
     //use mysql if statement if status = 1 then set to 0 else set to 1
@@ -456,11 +474,14 @@ app.post("/api/user/create", async (req, res) => {
   const password = req.body.password;
   const email = req.body.email;
   //Check for the username existing
-  let sql = 'SELECT count(*) FROM Users WHERE Username = ?'
-  const [result] = await pool.query(sql, [username])
+  let sql = "SELECT count(*) FROM Users WHERE Username = ?";
+  const [result] = await pool.query(sql, [username]);
   //Create hash with crypto sha256
   if (result[0]["count(*)"] == 0) {
-    const passwordHash = crypto.createHash('sha256').update(password).digest('hex');
+    const passwordHash = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
     sql = `INSERT INTO Users (Username, Password, Email) VALUES (?, ?, ?)`;
     pool.query(sql, [username, passwordHash, email]);
     res.sendStatus(200);
@@ -469,19 +490,25 @@ app.post("/api/user/create", async (req, res) => {
     res.sendStatus(409);
     Logs(req, 409);
   }
-})
+});
 
 app.post("/api/user/login", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  const passwordHash = crypto.createHash('sha256').update(password).digest('hex');
+  const passwordHash = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
   let sql = `SELECT * FROM Users WHERE Username = ? AND Password = ?`;
-  const [result] = await pool.query(sql, [username, passwordHash])
+  const [result] = await pool.query(sql, [username, passwordHash]);
   if (result.length == 1) {
     //Make a cookie with the username current time and a random number
     const date = new Date();
     const datetime = date.toISOString().slice(0, 19).replace("T", " ");
-    const cookie = crypto.createHash('sha256').update(username + datetime + crypto.randomBytes(16).toString("hex")).digest('hex');
+    const cookie = crypto
+      .createHash("sha256")
+      .update(username + datetime + crypto.randomBytes(16).toString("hex"))
+      .digest("hex");
     sql = `INSERT INTO Sessions (Cookie, UserID) VALUES (?, ?)`;
     await pool.query(sql, [cookie, result[0].ID]);
     res.cookie("Auth", cookie);
@@ -496,7 +523,7 @@ app.post("/api/user/login", async (req, res) => {
 app.get("/api/user", async (req, res) => {
   const cookie = req.cookies.Auth;
   let sql = `SELECT * FROM Sessions WHERE Cookie = ?`;
-  let [result] = await pool.query(sql, [cookie])
+  let [result] = await pool.query(sql, [cookie]);
   if (result.length == 1) {
     sql = `SELECT Username,Sudo FROM Users WHERE id = ?`;
     [result] = await pool.query(sql, [result[0].UserID]);
